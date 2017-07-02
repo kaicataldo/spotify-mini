9ffcd4faabaa8eb985ee385043a1507c54cfadaa
Kai Cataldo <kaicataldo@gmail.com>
2017-07-02 02:04:38 -0400

    Show info for current track

--- src/components/player/NowPlaying.vue
+++ src/components/player/NowPlaying.vue
@@ -1,11 +1,25 @@
 <template>
   <div>
-    <h1>NowPlaying</h1>
+    <h1>Now Playing:</h1>
+    <p>Artist: {{ artist }}</p>
+    <p>Album: {{ album }}</p>
+    <p>Track: {{ track }}</p>
   </div>
 </template>
 
 <script>
 export default {
   name: "now-playing",
+  computed: {
+    artist() {
+      return this.$store.state.app.artist_name;
+    },
+    album() {
+      return this.$store.state.app.album_name;
+    },
+    track() {
+      return this.$store.state.app.track_name;
+    }
+  }
 };
 </script>
