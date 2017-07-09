24e09553260a16946a0abed6f0ed60f0d19e478c
Kai Cataldo <kaicataldo@gmail.com>
2017-07-09 01:28:49 -0400

    Add tray icon

--- main/index.js
+++ main/index.js
@@ -3,11 +3,17 @@ const url = require("url");
 const electron = require("electron");
 const spotify = require("./lib/spotify");
 
-const { app, BrowserWindow, ipcMain } = electron;
-// Keep a global reference to mainWindow to prevent garbage collection from ending the process.
+const { app, BrowserWindow, Tray, ipcMain } = electron;
+// Keep a global reference to mainWindow/tray to prevent garbage collection from ending the process.
 let mainWindow;
+let tray;
+
+app.dock.hide();
 
 app.on("ready", () => {
+  tray = new Tray(path.join(__dirname, '../assets/icon.png'));
+  tray.setToolTip('Spotify Mini\nA macOS menubar controller for Spotify!');
+
   mainWindow = new BrowserWindow({ width: 400, height: 600, frame: false });
   mainWindow.loadURL(
     url.format({
@@ -18,18 +24,18 @@ app.on("ready", () => {
   );
 
   mainWindow.on("closed", () => {
-    // Clean up global variable to end process.
     mainWindow = null;
+    tray = null;
   });
+});
 
-  ipcMain.on("command", async (event, command) => {
-    let response;
-    try {
-      response = await spotify[command]();
-    } catch ({ message }) {
-      response = { status: "error", message };
-      console.error(response);
-    }
-    event.sender.send("setState", response);
-  });
+ipcMain.on("command", async (event, command) => {
+  let response;
+  try {
+    response = await spotify[command]();
+  } catch ({ message }) {
+    response = { status: "error", message };
+    console.error(response);
+  }
+  event.sender.send("setState", response);
 });
